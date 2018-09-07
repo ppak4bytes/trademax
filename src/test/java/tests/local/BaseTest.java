f24e64e09e1dc5b33aa83c401cf53c5e951934f7
pavlopak <pavlo.pak@forbytes.com>
2018-09-07 09:05:03 +0300

    commit --head

--- src/test/java/tests/local/BaseTest.java
+++ src/test/java/tests/local/BaseTest.java
@@ -12,8 +12,8 @@ public abstract class BaseTest {
     @BeforeClass(alwaysRun = true)
     public void beforeRun(){
         Configuration.browser = "chrome";
-        //Configuration.headless = true;
-        Configuration.startMaximized = true;
+        Configuration.headless = true;
+        //Configuration.startMaximized = true;
         Configuration.screenshots = false;
     }
 
