a98aba919f929876e5948abb960a1dbf505daf97
pavlopak <pavlo.pak@forbytes.com>
2018-09-11 12:32:25 +0300

    ++ making browser headless

--- src/test/java/tests/local/BaseTest.java
+++ src/test/java/tests/local/BaseTest.java
@@ -10,7 +10,7 @@ public abstract class BaseTest {
     @BeforeClass(alwaysRun = true)
     public void beforeRun(){
         //Configuration.browser = "chrome";
-
+        Configuration.headless = true;
         Configuration.startMaximized = true;
         Configuration.screenshots = false;
     }
